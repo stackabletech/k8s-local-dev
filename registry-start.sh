1b9474969f549cb977226ffa642654d6d8402dda
Nick Larsen <nick.larsen@stackable.tech>
2025-06-25 10:44:16 +0200

    fix: Use portable shebang, and enable bash strict mode

--- registry-start.sh
+++ registry-start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env -S bash -euo pipefail
 
 cd "$(dirname "$0")" || exit
 echo "Starting Zot Registry Cache..."
--- registry-stop.sh
+++ registry-stop.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env -S bash -euo pipefail
 cd "$(dirname "$0")" || exit
 
 echo "Stopping Zot Registry Cache..."
