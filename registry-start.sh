271ff4f22193abbcb99022fcf5ee152a3a141592
Nick Larsen <nick.larsen@stackable.tech>
2025-06-25 10:47:29 +0200

    chore: Use cat and heredoc to reduce the number of commands and visual noise

--- registry-start.sh
+++ registry-start.sh
@@ -6,11 +6,13 @@ pushd "$(dirname "$0")"
 echo "Starting Zot Registry Cache..."
 docker-compose up -d
 
-echo "Zot Registry Cache started!"
-echo "Registry UI:  http://localhost:5000"
-echo "Health check: http://localhost:5000/v2/_catalog"
-echo "Metrics:      http://localhost:5000/metrics"
-echo ""
-echo ""
-echo "To use with k3d clusters, run:"
-echo "k3d cluster create --config k3d-config.yaml [cluster-name]"
+cat <<EOF
+Zot Registry Cache started!
+Registry UI:  http://localhost:5000
+Health check: http://localhost:5000/v2/_catalog
+Metrics:      http://localhost:5000/metrics
+
+
+To use with k3d clusters, run:
+k3d cluster create --config k3d-config.yaml [cluster-name]
+EOF
